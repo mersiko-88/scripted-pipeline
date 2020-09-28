10071509dfc5d1b517c8bd80b5255a6a9bf7b7e2
mersiko-88 <69086912+mersiko-88@users.noreply.github.com>
2020-09-27 21:23:57 -0400

    Create prepare-worker.groovy

---
+++ prepare-worker.groovy
@@ -0,0 +1,21 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'input node IP', name: 'SSHNODE', trim: true)
+        ])
+
+    ])
+node {
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]){
+        stage("Initialize") {
+        
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.SSHNODE} yum install epel-release -y "
+        }
+        stage("Install java ") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.SSHNODE} yum install java-1.8.0-openjdk-devel  -y"
+        }
+        stage("Install git ") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.SSHNODE} yum install  git -y"    
+        
+        }
+    } 
+}
